c6d880d572b1023714344fdc94e569f0439d7058
Tanzhu Azis <tanjuozen90@gmail.com>
2018-02-16 18:19:11 +0100

    Refactoring the code and adding the missing sum method

--- chapter4-objects_and_arrays/sum-of-range.js
+++ chapter4-objects_and_arrays/sum-of-range.js
@@ -1,41 +1,33 @@
-function range(start, end) {
-    var increment;
+function range(start, end, step) {
     var isDirectionNormal = (start < end) ? true : false;
     var arr = [];
-    if (arguments.length === 3) {
-        increment = arguments[2];
-    } else {
-        if(isDirectionNormal){
-            increment = 1;
+    if(step == null){
+        if(start < end){
+            step = 1;
         } else {
-            increment = -1;
+            step = -1;
         }
     }
-    var isIncrementNegative = increment < 0 ? true : false;
     // check how the sequence will be, whether end is smaller than start
-    if (isDirectionNormal) {
-        console.log("start is smaller than end, normal loop");
-        if (isIncrementNegative) {
-            console.log("increment cannot be negative when the target end is higher than start");
-            return null;
-        }
-        for (var i = start; i <= end; i += increment) {
+   if(step > 0){
+        for(var i = start; i <= end; i += step){
             arr.push(i);
         }
-        return arr;
-    } else {
-        console.log("start is higher than end, inverse loop");
-        if (!isIncrementNegative) {
-            console.log("increment cannot be positive when the target end is smaller than start");
-            return null;
-        }
-        for (var i = start; i >= end; i += increment) {
+   } else {
+       for(var i = start; i >= end; i += step){
             arr.push(i);
-        }
-        return arr;
-    }
+       }
+   }
+   return arr;
 }
 
+function sum(arr) {
+    var total = 0;
+    for(var i = 0; i < arr.length; i++){
+        total += arr[i];
+    }
+    return total;
+}
 
 
 
